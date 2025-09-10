29412f4c881f7c3678840eb9306fb6f0efe5213b
Sumit <sk990005@outlook.com>
2025-09-10 18:35:35 +0530

    Add Services section

--- app/page.js
+++ app/page.js
@@ -1,19 +1,21 @@
 import Footer from './home/Footer';
 import Header from './home/Header';
 import Products from './home/Products';
-import Services from './home/Services';
+import Categories from './home/Categories';
 import Stats from './home/Stats';
 import Values from './home/Values';
+import Services from './home/Services';
 
 export default function Home() {
 	return (
 		<div className="flex flex-col ">
 			<Header />
 			<main className="flex-1 max-w-[1240px] mx-auto w-full h-full">
-				<Services />
+				<Categories />
 				<Products />
 				<Values />
 				<Stats />
+				<Services />
 			</main>
 			<Footer />
 		</div>
