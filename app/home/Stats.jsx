fffd26ae6a817ed6516bd4b5fcd815e95b3a9c5d
Sumit <sk990005@outlook.com>
2025-09-10 16:01:09 +0530

    add stats section

---
+++ app/home/Stats.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const STATS = [
+	{
+		id: 1,
+		value: 95,
+		description: 'Success rate in delivering products',
+		bg: 'bg-[#19417C]',
+	},
+	{ id: 2, value: 5, description: 'Wasetd materials', bg: 'bg-[#04724D]' },
+	{
+		id: 3,
+		value: 98,
+		description: 'Orders delivered on time',
+		bg: 'bg-[#19417C]',
+	},
+];
+
+const Stat = ({ bg, stat, description }) => {
+	return (
+		<div
+			className={`flex flex-col items-center py-26 px-13 flex-1 h-full ${bg}`}
+		>
+			<h2 className="font-primary text-[84px] font-bold">{stat}%</h2>
+			<p className="font-secondary text-2xl font-normal text-center">
+				{description}
+			</p>
+		</div>
+	);
+};
+
+const Stats = () => {
+	return (
+		<section className="flex items-center justify-stretch h-[374px]">
+			{STATS.map((stat) => (
+				<Stat bg={stat.bg} stat={stat.value} description={stat.description} />
+			))}
+		</section>
+	);
+};
+
+export default Stats;
--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
-import { Button } from '@/components/ui/button';
-import Header from './home/Header';
 import Footer from './home/Footer';
+import Header from './home/Header';
+import Products from './home/Products';
+import Services from './home/Services';
+import Stats from './home/Stats';
+import Values from './home/Values';
 
 export default function Home() {
 	return (
 		<div className="flex flex-col ">
 			<Header />
-			<main className="flex-1 max-w-[1240px] mx-auto w-full">
-				<h1>Main Content</h1>
+			<main className="flex-1 max-w-[1240px] mx-auto w-full h-full">
+				<Services />
+				<Products />
+				<Values />
+				<Stats />
 			</main>
 			<Footer />
 		</div>
