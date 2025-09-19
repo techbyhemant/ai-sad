63acd0227e0ccd8ab51b2d546bded93a1cd2216c
Sumit <sk990005@outlook.com>
2025-09-19 18:55:18 +0530

    Make products section responsive

--- app/about-us/components/Products.jsx
+++ app/about-us/components/Products.jsx
@@ -1,27 +1,67 @@
 import React from 'react';
+import hinge from '../../../public/assets/images/hinge.jpg';
 import building from '../../../public/assets/images/building.jpg';
 import PrimaryBtn from '@/app/shared/Buttons/PrimaryBtn';
+import { PiInfo } from 'react-icons/pi';
+import Image from 'next/image';
+
+const PRODUCTS = [
+	{ id: 1, img: hinge, title: 'W Hinges' },
+	{ id: 2, img: hinge, title: 'W Hinges' },
+	{ id: 3, img: hinge, title: 'W Hinges' },
+	{ id: 4, img: hinge, title: 'W Hinges' },
+];
 
 const Products = () => {
 	return (
-		<div
-			style={{
-				backgroundImage: `url(${building.src})`,
-				backgroundSize: 'cover',
-				backgroundRepeat: 'no-repeat',
-				backgroundPosition: 'center',
-			}}
-			className="py-[71px] px-[57px] h-[712px] w-full relative"
-		>
-			<div className="flex flex-col items-start gap-4 absolute z-10">
-				<p className="font-secondary text-md">Get to know</p>
-				<h4 className="font-secondary font-semibold text-4xl">BAONE</h4>
-				<PrimaryBtn title="Know more" styles="bg-white text-primary" />
+		<div className="h-[712px] w-full relative flex flex-col md:flex-row">
+			<div className="relative flex-1 h-[350px] md:h-auto">
+				<Image
+					src={building}
+					alt="Building"
+					fill
+					className="object-cover"
+					priority
+				/>
+
+				{/* Overlay */}
+				<div className="absolute inset-0 bg-gradient-to-t md:bg-gradient-to-b from-black/70 via-black/40 to-transparent pointer-events-none" />
+
+				<div className="absolute inset-0 z-20 flex items-end justify-between md:flex-col md:items-start md:justify-start p-4 md:p-8">
+					<div className="space-y-1">
+						<p className="font-secondary text-xs md:text-md text-white">
+							Get to know
+						</p>
+						<h4 className="font-secondary font-semibold text-xl md:text-4xl text-white">
+							BAONE
+						</h4>
+					</div>
+					<PrimaryBtn title="Know more" styles="bg-white text-primary" />
+				</div>
 			</div>
-			<div className="bg-[#04724D] h-full w-1/2 absolute right-0 top-0 z-10">
-                
-            </div>
-			<div className="absolute top-0 left-0 h-full w-full bg-gradient-to-b from-black to-white/0"></div>
+
+			{/* Right Side: Products Section */}
+			<div className="flex-1 bg-primary-green h-full grid grid-cols-2 grid-rows-2 gap-4 p-4">
+				{PRODUCTS.map((item) => (
+					<div
+						key={item.id}
+						className="flex flex-col items-start w-full gap-4 text-white"
+					>
+						<div className="relative w-[166px] h-[142px] md:w-40 md:h-40">
+							<Image
+								src={item.img}
+								alt={item.title}
+								fill
+								className="object-cover rounded-sm"
+							/>
+						</div>
+						<div className="flex items-center justify-between w-full">
+							<p className="text-md font-medium font-secondary">{item.title}</p>
+							<PiInfo />
+						</div>
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
